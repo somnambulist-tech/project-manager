ff22cb15d60f808a780dec268867768c11fac0e3
Dave Redfern <david.redfern@madagasgar.com>
2019-12-13 03:03:56 -0500

    Add compiler

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -11,7 +11,9 @@ if (file_exists($envFile)) {
     (new Dotenv(false))->loadEnv($envFile);
 }
 
-$_SERVER                                     += $_ENV;
-$_SERVER['APP_ENV']                          = 'dev';
-$_SERVER['APP_DEBUG']                        = true;
+$_SERVER += $_ENV;
+$_SERVER['APP_ENV']   = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
+$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+
 $_SERVER['SOMNAMBULIST_PROJECTS_CONFIG_DIR'] = dirname($envFile);
