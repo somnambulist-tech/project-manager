8dbe67765625f22875159024f0801635968e6749
Dave Redfern <dave-redfern@users.noreply.github.com>
2023-01-25 13:50:19 -0500

    Update to Symfony 6.2 and require PHP 8.1+

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -19,7 +19,7 @@ foreach ([$confDir, 'spm_projects.d', '.config/spm_projects.d', '.spm_projects.d
 
 if (!is_null($envFile)) {
     // load all the .env files
-    (new Dotenv(false))->loadEnv($envFile);
+    (new Dotenv())->loadEnv($envFile, defaultEnv: 'prod');
 } else {
     if (!in_array('init', $_SERVER['argv'])) {
         echo "\n ** Project Manager has not been initialised, run spm init **\n\n";
@@ -27,7 +27,7 @@ if (!is_null($envFile)) {
 }
 
 $_SERVER += $_ENV;
-$_SERVER['APP_ENV']   = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
+$_SERVER['APP_ENV']   = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'prod';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
 
